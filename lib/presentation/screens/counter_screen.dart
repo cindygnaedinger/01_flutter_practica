22b5e37b7d99fdadefad2d1a90e261a6d32e6083
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2024-02-20 08:33:52 -0300

    Crea app móvil para contar con botones de incremento, decremento y restart

---
+++ lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Counter Screen"), actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+        ]),
+        body: Center(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "$clickCounter",
+                  style: const TextStyle(
+                      fontSize: 160, fontWeight: FontWeight.w100),
+                ),
+                Text(
+                  clickCounter == 1 ? "Click" : "Clicks",
+                  style: const TextStyle(fontSize: 25),
+                )
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.refresh_rounded,
+              onPressed: () {
+                clickCounter = 0;
+                setState(() {});
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomButton(
+              icon: Icons.exposure_minus_1_outlined,
+              onPressed: () {
+                if (clickCounter == 0) return;
+                clickCounter--;
+                setState(() {});
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                clickCounter++;
+                setState(() {});
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+
+  final VoidCallback? onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      // shape: const StadiumBorder(),
+      enableFeedback: true,
+      elevation: 8,
+      backgroundColor: Colors.lightBlueAccent,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
